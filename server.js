860d068b1f0038764aa01ffb4b4e03ddef160bb9
nsmith12345 <nsmith_2003@hotmail.com>
2022-04-26 07:57:36 -0400

    final code for lesson 1

--- server.js
+++ server.js
@@ -45,6 +45,8 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
+  
+
 // function filterByQuery(query, animalsArray) {
 //     let filteredResults = animalsArray;
 //     if (query.diet) {
@@ -60,11 +62,16 @@ function filterByQuery(query, animalsArray) {
 
 // }
 
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-});
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+       res.json(result);
+    } else {
+        res/RTCRtpSender(404);
+    }    
+    });
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
